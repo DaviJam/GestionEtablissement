3e732118d8ba49cec80b4c1488a6a4a8a858b328
stevenmvn <pro.morvan.steven@gmail.com>
2021-01-05 16:20:34 +0100

    Business - Class School

--- src/ensup/business/School.java
+++ src/ensup/business/School.java
@@ -1,23 +1,52 @@
 package ensup.business;
 
+/**
+ * The type School.
+ */
 public class School extends Entity
 {
     private Director director;
 
-    public School(String surname, String mailAdress, String adress, int phoneNumber, Director director) {
-        super(surname, mailAdress, adress, phoneNumber);
+    /**
+     * Instantiates a new School.
+     *
+     * @param surname     the surname
+     * @param mailAddress  the mail address
+     * @param address     the address
+     * @param phoneNumber the phone number
+     * @param director    the director
+     */
+    public School(String surname, String mailAddress, String address, int phoneNumber, Director director) {
+        super(surname, mailAddress, address, phoneNumber);
         this.director = director;
     }
 
-    public School(String surname, String mailAdress, Director director) {
-        super(surname, mailAdress);
+    /**
+     * Instantiates a new School.
+     *
+     * @param surname     the surname
+     * @param mailAddress the mail address
+     * @param director    the director
+     */
+    public School(String surname, String mailAddress, Director director) {
+        super(surname, mailAddress);
         this.director = director;
     }
 
+    /**
+     * Gets director.
+     *
+     * @return the director
+     */
     public Director getDirector() {
         return director;
     }
 
+    /**
+     * Sets director.
+     *
+     * @param director the director
+     */
     public void setDirector(Director director) {
         this.director = director;
     }
