62f1ce388e619490a80b639844fe7c8562265871
Allan <semin.allan@gmail.com>
2021-01-05 16:23:59 +0100

    Add documentation for Teacher

--- src/ensup/business/Teacher.java
+++ src/ensup/business/Teacher.java
@@ -2,31 +2,42 @@ package ensup.business;
 
 /**
  * The type Teacher.
- * 
+ *
  * @author Youness
  */
 public class Teacher extends Person{
     private String subjectTaught;
 
+
     /**
-     * Instantiates a new Director.
+     * Instantiates a new Teacher.
      *
      * @param surname     the surname
-     * @param mailAdress  the mail adress
-     * @param adress      the adress
+     * @param mailAddress the mail address
+     * @param address     the address
      * @param phoneNumber the phone number
-     * @param id the id
-     * @param name the name
-     * @param password the password
+     * @param id          the id
+     * @param name        the name
+     * @param password    the password
      */
     public Teacher(String surname, String mailAddress, String address, int phoneNumber, int id, String name, String password) {
         super(surname, mailAddress, address, phoneNumber, id, name, password);
     }
 
+    /**
+     * Get subject taught string.
+     *
+     * @return the string
+     */
     public String getSubjectTaught(){
         return subjectTaught;
     }
 
+    /**
+     * Set subject taught.
+     *
+     * @param subjectTaught the subject taught
+     */
     public void setSubjectTaught(String subjectTaught){
         this.subjectTaught = subjectTaught;
     }
