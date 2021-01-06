2ca8f2a03f71f1cee0ff8174d51b3ca670407b2e
YounessT28 <youness281tab@gmail.com>
2021-01-06 12:21:21 +0100

    Update IService & Add IEntityService / ServicePerson

--- src/ensup/service/IService.java
+++ src/ensup/service/IService.java
@@ -3,8 +3,6 @@ package ensup.service;
 import ensup.business.Role;
 
 public interface IService<T> {
-    void Create(T entity, Role role);
-    void Update(T entity, Role role);
-    void Remove (T entity, Role role);
-    T Read(int index, Role role);
+    void Remove (T entity);
+    T Read(int index);
 }
