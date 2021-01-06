a96f6e14fee1d6d8ae6bcddc2655f4a53763e771
Amelie <nioche.amelie@gmail.com>
2021-01-06 12:27:10 +0100

    Update courseDao

--- src/ensup/dao/CourseDao.java
+++ src/ensup/dao/CourseDao.java
@@ -5,16 +5,44 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import ensup.business.Course;
 
 public class CourseDao
-{	
-	public CourseDao(Connect connect)
+{
+	public static List<Course> readAll()
 	{
+		Connection cn = Connect.openConnection();
+		List<Course> alCourse = new ArrayList<Course>();
+		
+		Statement st = null;
+		ResultSet res = null;
+		try
+		{
+			st = cn.createStatement();
+			res = st.executeQuery("SELECT * FROM Course");
+			while( res.next() )
+			{
+				Course cours = new Course(res.getString("subject"),res.getFloat("nbHours"),res.getInt("id"));
+				
+				alCourse.add(cours);
+			}
+		}
+		catch (SQLException e) {e.printStackTrace();}
+		finally{
+			try {
+				st.close();
+				cn.close();
+			}
+			catch(SQLException sqle) { sqle.printStackTrace(); }
+		}
+		
+		return alCourse;
 	}
 	
-	public Course get( int indice )
+	public static Course read( int indice )
 	{
 		Connection cn = Connect.openConnection();
 		Course cours = null;
@@ -40,7 +68,7 @@ public class CourseDao
 		return cours;
 	}
 	
-	public void add( String subject, float nbHours )
+	public static void create( String subject, float nbHours )
 	{
 		Connection cn = Connect.openConnection();
 		PreparedStatement pstmt = null;
@@ -62,5 +90,40 @@ public class CourseDao
 			}
 			catch(SQLException sqle) { sqle.printStackTrace(); }
 		}
+	}
+	
+	public static void delete( int indice )
+	{
+		if( indice != -1 && idExiste(indice) )
+		{
+			Connection cn = Connect.openConnection();
+			
+			Statement st = null;
+			try
+			{
+				st = cn.createStatement();
+				st.execute("DELETE FROM Course WHERE id="+indice);
+			}
+			catch (SQLException e) {e.printStackTrace();}
+			finally{
+				try {
+					st.close();
+					cn.close();
+				}
+				catch(SQLException sqle) { sqle.printStackTrace(); }
+			}
+		}
+	}
+	
+	public static boolean idExiste(int indice)
+	{
+		boolean existe = false;
+		
+		List<Course> alCourse = readAll();
+		for( Course cours : alCourse )
+			if( indice == cours.getId() )
+				existe = true;
+		
+		return existe;
 	}
 }
