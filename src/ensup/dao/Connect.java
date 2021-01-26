80c0855fb2bbdea84a4a408dc3e74b65fbbf4a78
David <daori971@gmail.com>
2021-01-26 15:01:30 +0100

    Udpate connection to database

--- src/ensup/dao/Connect.java
+++ src/ensup/dao/Connect.java
@@ -10,9 +10,9 @@ import java.sql.SQLException;
 public class Connect
 {
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://vps-0c0ccce5.vps.ovh.net:3306/GestionEtablissement";
-	private static final String USERNAME = "web";
-	private static final String PASSWORD = "Ensup2020*";
+	private static final String URL = "jdbc:mysql://localhost:3306/gestionetablissement?serverTimezone=UTC";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
 
 	/**
 	 * Open an connention with the information in the class
