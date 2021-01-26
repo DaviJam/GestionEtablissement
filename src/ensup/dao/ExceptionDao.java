2be3a344e708579aa7beb96394a61bcb90f5cf26
Youness <youness281tab@gmail.com>
2021-01-26 15:30:47 +0100

    ExceptionDao

---
+++ src/ensup/dao/ExceptionDao.java
@@ -0,0 +1,11 @@
+package ensup.dao;
+
+public class ExceptionDao extends Exception{
+    public ExceptionDao(){
+        super();
+    }
+
+    public ExceptionDao(String s){
+        super(s);
+    }
+}
