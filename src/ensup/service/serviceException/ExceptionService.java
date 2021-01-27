4ee353406bbce2630993ada95a92df983e92f852
Cherif-Bachir <52203575+Cherif-Bachir@users.noreply.github.com>
2021-01-27 10:20:32 +0100

    commit package et class exception Srvice

---
+++ src/ensup/service/serviceException/ExceptionService.java
@@ -0,0 +1,13 @@
+package ensup.service.serviceException;
+
+public class ExceptionService extends Exception {
+
+    public ExceptionService(){
+        super();
+    }
+    public ExceptionService(String s){
+        super(s);
+    }
+
+
+}
