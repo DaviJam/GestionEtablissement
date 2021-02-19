d386da255921ef66550a0008855918a8745aed7f
Allan <semin.allan@gmail.com>
2021-02-19 10:10:26 +0100

    Modif gestionEtablissement

---
+++ GestionEtablissement/GestionEtablissement-dto/src/main/java/eu/ensup/gestionetablissement/dto/App.java
@@ -0,0 +1,13 @@
+package eu.ensup.gestionetablissement.dto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
